28ee1667ed9324668bc340891b5d9176364a3a93
Dhruv Khandelwal <137927657+24Dhruv@users.noreply.github.com>
2024-07-01 19:12:50 +0530

    Create LinearSearch.java

---
+++ LinearSearch.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.util.ArrayList;
+
+ public class search{
+    public static void main(String args[]){
+int N = sc.nextInt();
+int [] arr = new int[N];
+  for(int i=0;i<N;i++){
+      arr[i] = sc.nextInt();
+    }
+System.out.println("enter m");
+ int M = sc.nextInt();
+    for(int i=0;i<N;i++){
+        if (arr[i]== M) {
+            System.out.println(i);            
+        }
+    }
+}}
